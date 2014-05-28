73cb9f1816cb97e5f11d7319a5ea31165f4828b8
Patrick Menzel <pmmenzel@gmail.com>
2014-05-28 12:48:42 -0700

    Add user stories file

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,31 @@
+// My role in the group is:  Person No. 1 - Translate tests into user stories. 
+
+// Here is my part of the challenge:
+
+
+// Test 1
+// As a user I want to, know what test number this is and determine (assert) that sum is a function.  
+
+// Test 2
+// As a user I want to, know what test number this is and calculate the sum of a set of numbers with an odd length.
+
+// Test 3
+// As a user I want to, know what test number this is and calculate the sum of a set of numbers with an even length.
+
+// Test 4
+// As a user I want to, know what test number this is and determine (assert) that mean is a function.  
+
+// Test 5
+// As a user I want to, know what test number this is and calculate the average or mean of a set of numbers with an odd length.
+
+// Test 6
+// As a user I want to, know what test number this is and calculate the average or mean of a set of numbers with an even length.
+
+// Test 7
+// As a user I want to, know what test number this is and determine (assert) that median is a function.  
+
+// Test 8
+// As a user I want to, know what test number this is and calculate the median value of a set of numbers with an odd length.
+
+// Test 9
+// As a user I want to, know what test number this is and calculate the median value of a set of numbers with an even length.
